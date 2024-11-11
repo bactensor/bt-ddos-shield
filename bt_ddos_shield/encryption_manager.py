4f29cb9c7c30d784202413e32d5afebebff488fc
Grzegorz Leszczyński <grzegorz.leszczynski@firma.gg.pl>
2024-11-11 18:07:42 +0100

    feat: encryption manager refactored

---
+++ bt_ddos_shield/encryption_manager.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+import ecies
+
+from bt_ddos_shield.utils import PublicKey, PrivateKey
+
+
+class EncryptionManagerException(Exception):
+    pass
+
+
+class EncryptionError(EncryptionManagerException):
+    pass
+
+
+class DecryptionError(EncryptionManagerException):
+    pass
+
+
+class AbstractEncryptionManager(ABC):
+    """
+    Abstract base class for manager handling manifest file encryption.
+    """
+
+    @abstractmethod
+    def encrypt(self, public_key: PublicKey, data: bytes) -> bytes:
+        """
+        Encrypts given data using the provided public key.
+        """
+        pass
+
+    @abstractmethod
+    def decrypt(self, private_key: PrivateKey, data: bytes) -> bytes:
+        """
+        Decrypts given data using the provided private key.
+        """
+        pass
+
+
+class ECIESEncryptionManager(AbstractEncryptionManager):
+    """
+    Encryption manager implementation using ECIES algorithm.
+    """
+
+    def encrypt(self, public_key: PublicKey, data: bytes) -> bytes:
+        try:
+            return ecies.encrypt(public_key, data)
+        except Exception as e:
+            raise EncryptionError(f"Encryption failed: {e}")
+
+    def decrypt(self, private_key: PrivateKey, data: bytes) -> bytes:
+        try:
+            return ecies.decrypt(private_key, data)
+        except Exception as e:
+            raise DecryptionError(f"Decryption failed: {e}")
