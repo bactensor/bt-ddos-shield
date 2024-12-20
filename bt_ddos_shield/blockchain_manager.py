4d14eb6ef8f502ed19492a528d71720464e18cf1
Tomasz Żyźniewski <zyzniewski@gmail.com>
2024-12-20 13:55:32 +0100

    feat: Bittensor BlockchainManager using commitments of knowledge

--- bt_ddos_shield/blockchain_manager.py
+++ bt_ddos_shield/blockchain_manager.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
+import bittensor
+import bittensor_wallet
+from bittensor.core.extrinsics.serving import (
+    get_metadata,
+    publish_metadata,
+)
+
 from bt_ddos_shield.address import (
     AbstractAddressSerializer,
     Address,
@@ -56,3 +63,64 @@ class AbstractBlockchainManager(ABC):
         Get data from blockchain for given user identified by hotkey or None if not found.
         """
         pass
+
+
+class BittensorBlockchainManager(AbstractBlockchainManager):
+    """
+    Bittensor BlockchainManager implementation using commitments of knowledge as storage.
+    """
+
+    def __init__(
+        self,
+        miner_hotkey: Hotkey,
+        address_serializer: AbstractAddressSerializer,
+        subtensor: bittensor.Subtensor,
+        wallet: bittensor_wallet.Wallet,
+        netuid: int,
+    ):
+        super().__init__(miner_hotkey, address_serializer)
+
+        self.subtensor = subtensor
+        self.wallet = wallet
+        self.netuid = netuid
+
+    def get(self, hotkey: Hotkey) -> Optional[bytes]:
+        """
+        Get data from blockchain for given user identified by hotkey or None if not found.
+        """
+
+        metadata = get_metadata(
+            self.subtensor,
+            self.netuid,
+            hotkey,
+        )
+
+        try:
+            field = metadata["info"]["fields"][0]
+        except TypeError:
+            return None
+        except LookupError:
+            return None
+
+        try:
+            value = next(iter(field.values()))
+        except StopIteration:
+            return None
+
+        return bytes.fromhex(value[2:])
+
+    def put(self, hotkey: Hotkey, data: bytes):
+        """
+        Put data to blockchain for given user identified by hotkey.
+        """
+
+        if hotkey != self.wallet.hotkey.ss58_address:
+            raise ValueError("Hotkey not in Wallet")
+
+        publish_metadata(
+            self.subtensor,
+            self.wallet,
+            self.netuid,
+            f"Raw{len(data)}",
+            data,
+        )
