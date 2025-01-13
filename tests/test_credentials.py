65f918c9b0f6c646ac0b99866ae4275b20de27a4
Grzegorz Leszczynski <grzegorz.leszczynski@reef.pl>
2025-01-13 13:33:34 +0100

    Update test credentials template

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -3,7 +3,7 @@ aws_access_key_id: str = ""
 aws_secret_access_key: str = ""
 aws_region_name: str = ""
 aws_s3_bucket_name: str = ""
-sql_alchemy_db_url: str = ""
+sql_alchemy_db_url: str = "sqlite:///ddos_shield.db"
 aws_route53_hosted_zone_id: str = ""
 aws_route53_other_hosted_zone_id: str = ""
 
